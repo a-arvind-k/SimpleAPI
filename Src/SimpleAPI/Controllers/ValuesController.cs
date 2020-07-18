0d375308e0624ffcc8b8cb42a47996452ef6c766
Arvind <kumar.arvind@accenture.com>
2020-07-18 15:52:57 +0530

    Fix the code

--- Src/SimpleAPI/Controllers/ValuesController.cs
+++ Src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Arv1";
+            return "Arv";
         }
 
         // POST api/values
